d39584a091be02f673adca48ba2325cee7bec9ce
milanher <milan.hermange@bettyblocks.com>
2024-10-03 12:42:25 +0200

    yuh

--- src/interactions/hello.ts
+++
@@ -1,3 +0,0 @@
-function hello({ name }: { name: string }): string {
-  return `Hello ${name || 'World'}!`;
-}
--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-import { showIfTrue } from '@betty-blocks/component-sdk';
-
-export const showOn = (key: string) => ({
-  configuration: { condition: showIfTrue(key) },
-});
